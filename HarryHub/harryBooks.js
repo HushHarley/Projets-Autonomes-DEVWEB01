203721da0bb4e0baed8da4785f2180205c8f8e0c
HushHarley <rankinharley3@gmail.com>
2026-08-07 14:42:08 -0400

    cleaned a bit and starting to add reservation page

--- HarryHub/harryBooks.js
+++ HarryHub/harryBooks.js
@@ -14,8 +14,8 @@ function afficherData(donnees) {
     for (let index = 0; index < donnees.length; index++) {
         let div = document.createElement("div");
         div.classList = "bookCarte";
-        div.innerHTML = `<h2 class="name">${donnees[index].title}</h2>
-                        <p class="description">${donnees[index].description}</p>
+        div.innerHTML = `<h2 class="bookName">${donnees[index].title}</h2>
+                        <p class="bookDescription">${donnees[index].description}</p>
                         <img class="bookCover" src="${bookImages[index]}" alt="">
         `
 
@@ -24,14 +24,14 @@ function afficherData(donnees) {
 }
 
 const bookImages = [
-    "https://th.bing.com/th/id/OIP.v6D32I_4azNBbv3KinpQKwHaEK?w=291&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
-    "https://th.bing.com/th/id/R.ce18cc9989db0420fe42b8a09d0dd0a9?rik=%2fPD%2ftghxrx0jDQ&pid=ImgRaw&r=0",
-    "https://m.media-amazon.com/images/S/pv-target-images/9cd51c348b06fc970bcd7c55dc7920aef2a5b43c8f5e0050d39b4d3fc54bdb7e.jpg",
-    "https://th.bing.com/th/id/OIP.XWAvTNEJRBDRxrAjM9YjBAHaIo?w=169&h=197&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
-    "https://th.bing.com/th/id/OIP.hkwOw2jGbAQudD-wtB3zhQHaJ4?w=127&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
-    "https://th.bing.com/th/id/OIP.hoOcDxiXBmR3RYDlaKOfHQHaJ4?w=201&h=268&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
-    "https://www.themoviedb.org/t/p/original/bw2AAFfOa4p0IKamvprZUwDXv1h.jpg",
-    "https://th.bing.com/th/id/OIP.2Py5OesvlYyX05WCOKY4iAHaHa?w=200&h=200&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3"
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-1.jpg&w=1320&q=75",
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-2.jpg&w=1320&q=75",
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-3.jpg&w=1320&q=75",
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-4.jpg&w=1320&q=75",
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-5.jpg&w=1320&q=75",
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-6.jpg&w=1320&q=75",
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-7.jpg&w=1320&q=75",
+    "https://www.harrypotter.com/_next/image?url=%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-8.jpg&w=1320&q=75"
 ];
 
 // books.forEach(function(books, index) {
